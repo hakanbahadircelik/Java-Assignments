ff7f04928d7e2f3ce4675f53e6f3b5a044dab035
hakanbahadircelik <hakanbahadircelik@gmail.com>
2023-12-11 12:39:21 +0400

    Third homework answers.

---
+++ src/StringMethods/_01_StringMethods.java
@@ -0,0 +1,11 @@
+package StringMethods;
+
+public class _01_StringMethods {
+    public static void main(String[] args) {
+
+        String text = "I love java";
+        int textSize = text.length();
+        System.out.println("textSize = " + textSize);
+
+    }
+}
